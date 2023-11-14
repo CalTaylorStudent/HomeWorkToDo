e37bd1bf32d7406d34c476c83804c3b0731d7ded
CalTaylor <s0357576@student.rockvalleycollege.edu>
2023-11-14 10:57:24 -0600

    Initial Commit

---
+++ HomeWorkToDo/HomeWorkToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeWorkToDoApp.swift
+//  HomeWorkToDo
+//
+//  Created by Cal M. Taylor on 11/14/23.
+//
+
+import SwiftUI
+
+@main
+struct HomeWorkToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
